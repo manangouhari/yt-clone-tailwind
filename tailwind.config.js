59359886e7b948ff2538fc1b3a65c4398e004a81
Manan <gouhari21@hotmail.com>
2020-07-19 00:33:34 +0530

    95% done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      'roboto': ['Roboto', 'Arial']
+    },
+    extend: {
+     spacing: {
+       '14': '3.5rem',
+       '18': '4.5rem',
+       '36': '9rem',
+       '68': '17rem'
+     } 
+    },
+  },
+  variants: {},
+  plugins: [],
+}
